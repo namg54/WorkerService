6b464f0cb2d400c5f5343422ee053afb377a497b
namg54 <79947165+namg54@users.noreply.github.com>
2023-03-11 00:02:00 +0330

    Add project files.

---
+++ WorkerService/BackgroundWorkerService.cs
@@ -0,0 +1,19 @@
+﻿public class BackgroundWorkerService : BackgroundService
+{
+
+    readonly ILogger<BackgroundWorkerService> _logger;
+    public BackgroundWorkerService(ILogger<BackgroundWorkerService> logger)
+    {
+        _logger = logger;
+    }
+    
+
+    protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            _logger.LogInformation("NAMG Worker running at {time}", DateTimeOffset.Now);
+            await Task.Delay(1000, stoppingToken);
+        }
+    }
+}
